79c7749b71b59fad24042c90f66e00dbce88d261
Manogel <manoelgomes53@gmail.com>
2020-09-15 11:14:42 -0300

    refactor: section hero

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,21 @@
-export type LogoProps = {
+type ImageDataProps = {
   alternativeText: string
   url: string
 }
 
+export type LogoProps = ImageDataProps
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: ImageDataProps
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
